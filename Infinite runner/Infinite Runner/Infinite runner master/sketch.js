e92ea1456d9baf0de0e4783fb6ce543711134108
Aadithya Vasudevan <68108339+Aadithya-19@users.noreply.github.com>
2020-09-06 10:51:41 +0530

    Add files via upload

---
+++ Infinite runner/Infinite Runner/Infinite runner master/sketch.js
@@ -0,0 +1,37 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var plyr, plyr_Img;
+
+function preload(){
+
+  plyr_Img = loadImage("baby_robot.png");
+
+}
+
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 1){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
